8b5ee717546e06c35edba8d64811d6b0a85ce541
Johan <johan@romantech.net>
2021-03-18 18:15:53 +0900

    backup-2021-03-18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './store/store.js';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
 );
---
+++ src/reducers/initialState.js
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/prefer-default-export */
+export const initialState = 
+{
+  "projects": [
+    {
+      "projectId": 1,
+      "projectName": "우주 정복",
+      "projectDesc": "우주 정복을 위한 프로젝트입니다",
+      "endDate": "2021-03-28",
+      "image": ""
+    }
+  ],
+  "users": [
+    {
+      "userId": 1,
+      "email": "888@gmail.com", 
+      "name": "Smith", 
+      "description": "", 
+      "portfolio": ["조인어스", "당근마켓"], 
+      "available_stack": ["JavaScript", "CSS", "React"]
+    }
+  ]
+}
