3b27aa2284a9ceba447573e682746a9d3423bcbc
Kim Jae Woo <erick1129@naver.com>
2021-03-22 13:14:20 +0900

    [client] /#28/ feature: fix myProfile.js

--- src/component/myProfile.js
+++ src/component/myProfile.js
@@ -1,30 +1,22 @@
 import React, { useState, useEffect } from "react";
-import Axios from "axios";
 
-export default function Profile() {
-  const [userName, setuserName] = useState("김코딩");
-  const [userEmail, setuserEmail] = useState("kimcoding@codestate.com");
-  const [userStack, setuserStack] = useState([
-    "NodeJS",
-    "javascript",
-    "java",
-    "C#",
-  ]);
+export default function Profile({userInfo}) {
 
   const [userPhoto, setuserPhoto] = useState(
     "https://upload.wikimedia.org/wikipedia/commons/7/7e/Circle-icons-profile.svg"
   );
+
 
   return (
     <div className="myProfile-body">
       <h2>프로필</h2>
       <div className="myProfile-border">
         <div className="myProfile-left">
-          <h3>이름 : {userName}</h3>
-          <div>이메일 : {userEmail}</div>
+          <h3>이름 : {userInfo.userName}</h3>
+          <div>이메일 : {userInfo.userEmail}</div>
           <div className="tag_lists">
             개발언어 :
-            {userStack.map((stack_list) => {
+            {userInfo.stack.map((stack_list) => {
               return (
                 <div className="tag" key={stack_list}>
                   {stack_list}
@@ -34,14 +26,11 @@ export default function Profile() {
           </div>
           <h4>포트폴리오</h4>
           <div>
-            <ul>
-              <li>
-                <a href="">포플 링크</a>
-              </li>
-              <li>
-                <a href="">포플 링크</a>
-              </li>
-            </ul>
+            {userInfo.portfolio.map((link)=>{
+              return(
+                <a href={link} key={link} >{link}</a>
+              )
+            })}
           </div>
         </div>
         <div className="myProfile-right">
