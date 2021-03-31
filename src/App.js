570c8a5050079de5f2adfadf958ecf7b856da155
Johan <johan@romantech.net>
2021-03-31 20:42:42 +0900

    20210331-test04

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import ProjectUpdate from './components/ProjectUpdate';
 
 export default function App() {
   console.log('Hello World');
+  console.log('Hello World');
   const isLogin = useSelector(state => state.loginReducer.isLogin);
   return (
     <div className="App">
