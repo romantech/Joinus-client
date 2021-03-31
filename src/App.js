46151eb16dd448758828f79d18b1ca4756585128
Johan <johan@romantech.net>
2021-03-31 21:39:56 +0900

    20210331-feature02

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import ProjectUpdate from './components/ProjectUpdate';
 
 export default function App() {
   console.log('feature01');
+  console.log('feature02');
   const isLogin = useSelector(state => state.loginReducer.isLogin);
   return (
     <div className="App">
